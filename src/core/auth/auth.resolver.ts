837364ad81b0cec9a621ed734d1c4af8ba92687f
OUMAIMAtakrour <takrouroumaima@gmail.com>
2025-02-24 17:01:02 +0000

    Feat:login is working fine + the access token is generated and refresh token generated and stored in database correctly

--- src/core/auth/auth.resolver.ts
+++ src/core/auth/auth.resolver.ts
@@ -2,7 +2,8 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './types/user.type';
 import { AuthService } from './auth.service';
 import { SignupInput } from './inputs/signup.input';
-
+import { LoginInput } from './inputs/login.input';
+import { AuthResponse } from './types/auth-response.type';
 
 @Resolver(() => User)
 export class AuthResolver {
@@ -10,6 +11,10 @@ export class AuthResolver {
   @Mutation(() => User)
   async signup(@Args('input') signupInput: SignupInput) {
     return this.authService.signup(signupInput);
+  }
+  @Mutation(() => AuthResponse)
+  async login(@Args('input') loginInput: LoginInput) {
+    return this.authService.login(loginInput);
   }
   @Query(() => String)
   hello() {
--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -12,6 +12,8 @@ import { JwtService } from '@nestjs/jwt';
 import { RefreshToken } from 'src/core/auth/schemas/refresh-token.schema';
 import { v4 as uuidv4 } from 'uuid';
 import { SignupInput } from './inputs/signup.input';
+import { LoginInput } from './inputs/login.input';
+import { AuthResponse } from './types/auth-response.type';
 
 @Injectable()
 export class AuthService {
@@ -39,58 +41,56 @@ export class AuthService {
     });
     return createdUser;
   }
-  // async login(loginDto: LoginDto) {
-  //   const user = await this.UserModel.findOne({ email: loginDto.email });
+  async login(loginDto: LoginInput): Promise<AuthResponse> {
+    const user = await this.UserModel.findOne({ email: loginDto.email });
 
-  //   if (!user) {
-  //     throw new UnauthorizedException('Invalid credentials');
-  //   }
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
 
-  //   const isPasswordValid = await bcrypt.compare(
-  //     loginDto.password,
-  //     user.password,
-  //   );
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
 
-  //   if (!isPasswordValid) {
-  //     throw new UnauthorizedException('Invalid credentials');
-  //   }
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
 
-  //   const payload = {
-  //     userId: user._id.toString(),
-  //     email: user.email,
-  //     role: user.role,
-  //   };
+    // const payload = {
+    //   userId: user._id.toString(),
+    //   email: user.email,
+    //   role: user.role,
+    // };
+    const tokens = await this.generateUserTokens(user._id.toString());
+    return tokens;
+  }
+  async generateUserTokens(userId) {
+    const access_token = this.jwtService.sign({ userId }, { expiresIn: '2h' });
+    const refresh_token = uuidv4();
+    await this.storeRefreshToken(refresh_token, userId);
 
-  //   return {
-  //     access_token: this.jwtService.sign(payload),
-  //   };
-  // }
-  // async generateUserTokens(userId) {
-  //   const accessToken = this.jwtService.sign({ userId }, { expiresIn: '2h' });
-  //   const refreshToken = uuidv4();
-  //   await this.storeRefreshToken(refreshToken, userId);
+    return { access_token, refresh_token };
+  }
+  async refreshTokens(refreshToken: string) {
+    const token = await this.RefreshTokenModel.findOne({
+      token: refreshToken,
+      expiryDate: { $gte: new Date() },
+    });
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    return this.generateUserTokens(token.userId);
+  }
+  async storeRefreshToken(token: string, userId) {
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + 3);
+    console.log(userId);
+    console.log(token);
+    console.log(expiryDate);
 
-  //   return { accessToken, refreshToken };
-  // }
-  // async refreshTokens(refreshToken: string) {
-  //   const token = await this.RefreshTokenModel.findOne({
-  //     token: refreshToken,
-  //     expiryDate: { $gte: new Date() },
-  //   });
-  //   if (!token) {
-  //     throw new UnauthorizedException();
-  //   }
-  //   return this.generateUserTokens(token.userId);
-  // }
-  // async storeRefreshToken(token: string, userId) {
-  //   const expiryDate = new Date();
-  //   expiryDate.setDate(expiryDate.getDate() + 3);
-  //   console.log(userId);
-  //   console.log(token);
-  //   console.log(expiryDate);
-
-  //   await this.RefreshTokenModel.create({ token, userId, expiryDate });
-  // }
+    await this.RefreshTokenModel.create({ token, userId, expiryDate });
+  }
   // findAll() {
   //   return `This action returns all auth`;
   // }
