8f2a5e9d8a3253dd4edcebd4cb2db7789c13cfcb
OUMAIMAtakrour <takrouroumaima@gmail.com>
2025-02-27 10:23:15 +0000

    Feat:switching auth guard to gql context

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -8,15 +8,21 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
+import { Model } from 'mongoose';
+import { User } from 'src/core/auth/schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { GqlExecutionContext } from '@nestjs/graphql';
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const gqlContext = GqlExecutionContext.create(context);
+    const { req } = gqlContext.getContext();
+    const token = this.extractTokenFromHeader(req);
 
     if (!token) {
       throw new UnauthorizedException('No token provided');
@@ -27,11 +33,11 @@ export class AuthGuard implements CanActivate {
         secret: process.env.JWT_SECRET,
       });
 
-      request.user = {
-        userId: payload.userId,
-        role: payload.role,
-      };
-
+      const user = await this.userModel.findById(payload.userId);
+      if (!user) {
+        throw new UnauthorizedException('User not foud');
+      }
+      req.user = user;
       return true;
     } catch (error) {
       console.error('Token verification error:', error);
@@ -39,8 +45,8 @@ export class AuthGuard implements CanActivate {
     }
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const authHeader = request.headers.authorization;
+  private extractTokenFromHeader(req: Request): string | undefined {
+    const authHeader = req.headers.authorization;
     if (!authHeader) return undefined;
 
     const [type, token] = authHeader.split(' ');
